ad79bcddbea80523a891362b3282a59685778ce1
ilovezfs <ilovezfs@icloud.com>
2016-10-28 08:18:51 -0700

    kat 2.2.0

--- kat.rb
+++ kat.rb
@@ -1,10 +1,9 @@
 class Kat < Formula
   desc "K-mer Analysis Toolkit (KAT) analyses k-mer spectra"
   homepage "https://github.com/TGAC/KAT"
-  url "https://github.com/TGAC/KAT/releases/download/Release-2.1.1/kat-2.1.1.tar.gz"
-  sha256 "bcb86a01bdd2aa01cc64c6d2f2a33fff1a71961867e45a21de8de303e6f3440d"
+  url "https://github.com/TGAC/KAT/releases/download/Release-2.2.0/kat-2.2.0.tar.gz"
+  sha256 "ea36dbf409900b84095ee9ee7e1bf34e105b24256eea2a45225794755e58daaf"
   # tag "bioinformatics"
-  revision 1
 
   bottle do
     cellar :any
