716089eae7d8197aff10529e14ca48e889eb9cab
ilovezfs <ilovezfs@icloud.com>
2017-04-03 05:25:15 -0700

    dotwrp 2.0

--- dotwrp.rb
+++ dotwrp.rb
@@ -1,18 +1,17 @@
 class Dotwrp < Formula
+  desc "Wrapper for CBLAS *dot* functions in Accelerate framework"
   homepage "https://github.com/tenomoto/dotwrp"
-  url "https://github.com/tenomoto/dotwrp/archive/v1.1.tar.gz"
-  sha256 "369101851ecdffe89895ff91d21e535ef2df9c00c186f9e4f9443c709dfd825d"
-  revision 1
-
+  url "https://github.com/tenomoto/dotwrp/archive/v2.0.tar.gz"
+  sha256 "e5dce5f75d6f941b7f0b13dcc4c140e8c90ea8f1a26dba0853d270a69a62737e"
   head "https://github.com/tenomoto/dotwrp.git"
 
   depends_on :fortran
 
   def install
     # note: fno-underscoring is vital to override the symbols in Accelerate
-    system "#{ENV.fc} #{ENV.fflags} -fno-underscoring -c dotwrp.f90"
-    system "ar -cru libdotwrp.a dotwrp.o"
-    system "ranlib libdotwrp.a"
+    system ENV.fc, ENV.fflags, "-fno-underscoring", "-c", "dotwrp.f90"
+    system "ar", "-cru", "libdotwrp.a", "dotwrp.o"
+    system "ranlib", "libdotwrp.a"
 
     lib.install "libdotwrp.a"
   end
