a4283104d6687610ac96ab3d1b935f5cdbc4f286
ilovezfs <ilovezfs@icloud.com>
2017-05-01 13:05:58 -0700

    cd-hit 4.6.7

--- cd-hit.rb
+++ cd-hit.rb
@@ -4,9 +4,9 @@ class CdHit < Formula
   # doi "10.1093/bioinformatics/btl158"
   # tag "bioinformatics"
 
-  url "https://github.com/weizhongli/cdhit/releases/download/V4.6.6/cd-hit-v4.6.6-2016-0711.tar.gz"
-  version "4.6.6"
-  sha256 "97946f8ae62c3efa20ad4c527b8ea22200cf1b75c9941fb14de2bdaf1d6910f1"
+  url "https://github.com/weizhongli/cdhit/releases/download/V4.6.7/cd-hit-v4.6.7-2017-0501-source.tar.gz"
+  version "4.6.7"
+  sha256 "c39d3efe779580fbd664eedc28e5c508a3979f95634c92817d78f8b6d35d7a58"
 
   head "https://github.com/weizhongli/cdhit.git"
 
