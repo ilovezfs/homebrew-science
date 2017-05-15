15eb35675da07fc559aa7ec275808a1eb8e0dc91
ilovezfs <ilovezfs@icloud.com>
2017-05-15 03:03:31 -0700

    armadillo 7.900.1

--- armadillo.rb
+++ armadillo.rb
@@ -1,8 +1,8 @@
 class Armadillo < Formula
   desc "C++ linear algebra library"
   homepage "https://arma.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/arma/armadillo-7.800.2.tar.xz"
-  sha256 "efd70600ab6f30449d90a3e58ff7499937985209bb0a03ce4e322474dbf11fe3"
+  url "https://downloads.sourceforge.net/project/arma/armadillo-7.900.1.tar.xz"
+  sha256 "33eec7013990b5477ccc5ad3abc68bc2326c7a7a2790014d625cfcf37c0e07d3"
 
   bottle do
     cellar :any
