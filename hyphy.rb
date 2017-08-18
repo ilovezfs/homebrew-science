753f9ddb0f94ef35a1acabd505962298fbfbc429
ilovezfs <ilovezfs@icloud.com>
2017-08-17 20:43:58 -0700

    hyphy 2.3.0

--- hyphy.rb
+++ hyphy.rb
@@ -1,8 +1,8 @@
 class Hyphy < Formula
   desc "Hypothesis testing with phylogenies"
   homepage "http://www.hyphy.org/"
-  url "https://github.com/veg/hyphy/archive/2.2.7.tar.gz"
-  sha256 "8c84340665b126742b85ed8d54354455ffc97ebd3cc689658120d7f5791adf13"
+  url "https://github.com/veg/hyphy/archive/2.3.0.tar.gz"
+  sha256 "63d5ca3a05d5995800438de49713f1e88dc2b5a7a4fffe8bfb7b94c1f582af09"
   head "https://github.com/veg/hyphy.git"
 
   bottle do
@@ -17,14 +17,12 @@ class Hyphy < Formula
 
   depends_on "openssl"
   depends_on "cmake" => :build
-  depends_on mpi: :optional
+  depends_on :mpi => :optional
 
   fails_with :clang do
     build 77
     cause "cmake gets passed the wrong flags"
   end
-
-  patch :DATA # single-threaded builds
 
   def install
     system "cmake", "-DINSTALL_PREFIX=#{prefix}", ".", *std_cmake_args
@@ -48,66 +46,3 @@ class Hyphy < Formula
     system libexec/"HYPHYGTEST"
   end
 end
-
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 76228a8..ee4bb80 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -299,6 +299,23 @@ add_custom_target(MP2 DEPENDS HYPHYMP)
- 
- 
- #-------------------------------------------------------------------------------
-+# hyphy sp target
-+#-------------------------------------------------------------------------------
-+add_executable(
-+    HYPHYSP
-+    EXCLUDE_FROM_ALL
-+    ${SRC_COMMON} ${SRC_UNIXMAIN}
-+)
-+target_link_libraries(HYPHYSP ${DEFAULT_LIBRARIES})
-+install(
-+    TARGETS HYPHYSP
-+    RUNTIME DESTINATION bin
-+    OPTIONAL
-+)
-+add_custom_target(SP DEPENDS HYPHYSP)
-+
-+
-+#-------------------------------------------------------------------------------
- # hyphy OpenCL target
- #-------------------------------------------------------------------------------
- find_package(OpenCL)
-@@ -546,7 +563,7 @@ endif((${QT4_FOUND}))
- #-------------------------------------------------------------------------------
- if(UNIX)
-     set_property(
--        TARGET HYPHYMP hyphy_mp HYPHYGTEST HYPHYDEBUG
-+        TARGET HYPHYMP hyphy_mp HYPHYGTEST HYPHYDEBUG HYPHYSP
-         APPEND PROPERTY COMPILE_DEFINITIONS __UNIX__
-     )
- endif(UNIX)
-@@ -557,7 +574,7 @@ set_property(
- )
- 
- set_property(
--    TARGET hyphy_mp HYPHYMP HYPHYGTEST HYPHYDEBUG
-+    TARGET hyphy_mp HYPHYMP HYPHYGTEST HYPHYDEBUG HYPHYSP
-     APPEND PROPERTY COMPILE_DEFINITIONS _HYPHY_LIBDIRECTORY_="${CMAKE_INSTALL_PREFIX}/lib/hyphy"
- )
- 
-@@ -567,6 +584,13 @@ set_property(
- )
- 
- set_target_properties(
-+    HYPHYSP
-+    PROPERTIES
-+    COMPILE_FLAGS "${DEFAULT_COMPILE_FLAGS}"
-+    LINK_FLAGS "${DEFAULT_LINK_FLAGS}"
-+)
-+
-+set_target_properties(
-     hyphy_mp HYPHYMP
-     PROPERTIES
-     COMPILE_FLAGS "${DEFAULT_COMPILE_FLAGS} ${OpenMP_CXX_FLAGS}"
-
