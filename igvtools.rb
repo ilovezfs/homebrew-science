c0647a9f9ba7a6b6910c8070b2feb8664abf752a
ilovezfs <ilovezfs@icloud.com>
2017-12-23 23:57:51 -0800

    igvtools 2.4.5

--- igvtools.rb
+++ igvtools.rb
@@ -1,8 +1,8 @@
 class Igvtools < Formula
   desc "Utilities for preprocessing data files for IGV"
   homepage "https://www.broadinstitute.org/software/igv"
-  url "https://github.com/igvteam/igv/archive/v2.3.98.tar.gz"
-  sha256 "de22b1e646bd2290757e617377b1c598f419e292d28f049ede958d0f08bffde5"
+  url "https://github.com/igvteam/igv/archive/v2.4.5.tar.gz"
+  sha256 "8eeecd23491317b809d523191805533f2ce547fec62ddbd40b14736f61523f85"
   head "https://github.com/igvteam/igv.git"
 
   bottle do
