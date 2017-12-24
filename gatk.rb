f960478046f326d62e0f6930c91bcb59fd10b9a3
ilovezfs <ilovezfs@icloud.com>
2017-12-23 23:56:44 -0800

    gatk 3.8-1

--- gatk.rb
+++ gatk.rb
@@ -1,13 +1,12 @@
 class Gatk < Formula
   desc "Genome Analysis Toolkit: Variant Discovery in High-Throughput Sequencing"
   homepage "https://software.broadinstitute.org/gatk/"
-  url "https://github.com/broadgsa/gatk-protected/archive/3.6.tar.gz"
-  sha256 "f2f1311136e06189b0f44a35a3c92fa700ddcbf3d6c4760517ec7bbe08eafce0"
+  url "https://github.com/broadgsa/gatk-protected/archive/3.8-1.tar.gz"
+  version "3.8-1"
+  sha256 "ae96d018ab71a83b455bffb31a60bf9f78344d7b2b6ea94b604bd26cec49f5cd"
   head "https://github.com/broadgsa/gatk-protected.git"
   # doi "10.1101/gr.107524.110"
   # tag "bioinformatics"
-
-  bottle :disable, "See https://github.com/Homebrew/homebrew-science/pull/4428"
 
   depends_on :java
   depends_on "maven" => :build
