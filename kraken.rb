bb1d5d7092ade837348e7f0a0467879228fcf28f
ilovezfs <ilovezfs@icloud.com>
2017-12-24 00:00:09 -0800

    kraken 1.0

--- kraken.rb
+++ kraken.rb
@@ -1,8 +1,8 @@
 class Kraken < Formula
   desc "Assign taxonomic labels to short DNA sequences"
   homepage "https://ccb.jhu.edu/software/kraken/"
-  url "https://ccb.jhu.edu/software/kraken/dl/kraken-0.10.5-beta.tgz"
-  sha256 "7c0ac64ee0acdcce18e16b51b636b7cdc6d07ea7ab465bb64df078c5a710346b"
+  url "https://ccb.jhu.edu/software/kraken/dl/kraken-1.0.tgz"
+  sha256 "cec40d3545199d48388269fc1b7fda9f48ae3aa73f99ed4410e1474abe421e6a"
   head "https://github.com/DerrickWood/kraken.git"
   # doi "10.1186/gb-2014-15-3-r46"
   # tag "bioinformatics"
