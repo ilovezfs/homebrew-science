16637956869008e7f8464f29ebca3149d0df7251
ilovezfs <ilovezfs@icloud.com>
2017-12-24 00:15:22 -0800

    pykep 2.1

--- pykep.rb
+++ pykep.rb
@@ -1,12 +1,11 @@
 class Pykep < Formula
   desc "Library providing basic tools for astrodynamics research"
   homepage "https://esa.github.io/pykep/"
-  revision 2
   head "https://github.com/esa/pykep.git"
 
   stable do
-    url "https://github.com/esa/pykep/archive/v1.3.9.tar.gz"
-    sha256 "1bb0a71a1d19908e3703ca6e3cc87d75f2f8b91c65c3a778e1acd1962c1e87f1"
+    url "https://github.com/esa/pykep/archive/v2.1.tar.gz"
+    sha256 "601a73cc6090977091ae719e692dd55cc51bde241ba2f156fb06ac1b7130feca"
 
     # Remove for > 1.3.9
     # Upstream commit from 8 Feb 2017 "fix for #57 - linking to python libraries
@@ -17,8 +16,6 @@ class Pykep < Formula
       sha256 "6b2372c77b0e4062145d4bed6f372d3f04283c2ab58014f8994950832e56099e"
     end
   end
-
-  bottle :disable, "needs to be rebuilt with latest boost"
 
   depends_on "cmake" => :build
   depends_on "boost"
