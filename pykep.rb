dff552e758aef8818e47d56dc515e86522b27d58
ilovezfs <ilovezfs@icloud.com>
2017-12-25 03:54:24 -0800

    Update pykep.rb

--- pykep.rb
+++ pykep.rb
@@ -1,21 +1,9 @@
 class Pykep < Formula
   desc "Library providing basic tools for astrodynamics research"
   homepage "https://esa.github.io/pykep/"
+  url "https://github.com/esa/pykep/archive/v2.1.tar.gz"
+  sha256 "601a73cc6090977091ae719e692dd55cc51bde241ba2f156fb06ac1b7130feca"
   head "https://github.com/esa/pykep.git"
-
-  stable do
-    url "https://github.com/esa/pykep/archive/v2.1.tar.gz"
-    sha256 "601a73cc6090977091ae719e692dd55cc51bde241ba2f156fb06ac1b7130feca"
-
-    # Remove for > 1.3.9
-    # Upstream commit from 8 Feb 2017 "fix for #57 - linking to python libraries
-    # only for windows now"
-    # https://github.com/esa/pykep/commit/351b628bf404ed53b3ae3e06eb8bdc56dc1deb11
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/d42dc14/pykep/nopythonlinking.diff"
-      sha256 "6b2372c77b0e4062145d4bed6f372d3f04283c2ab58014f8994950832e56099e"
-    end
-  end
 
   depends_on "cmake" => :build
   depends_on "boost"
